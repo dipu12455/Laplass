0823830830f1c7cdcd8e8ef05bc2233316fa24ab
dgiri12 <dgiri1@msudenver.edu>
2023-05-27 14:27:11 -0600

    successfully implemented pixijs, figured out how to supply resources to client webpage

--- index.js
+++ index.js
@@ -11,11 +11,15 @@ var testVec = v1.getVector();
 
 console.log(`testVec: (${testVec.getX()}, ${testVec.getY()})`);
 
-
 app.get('/', (req, res) => {
-    res.send('Program running...');
+  res.sendFile(__dirname + '/index.html');
+});
+
+//this is how you supply resources to index.html that contains the pixijs script. the script works in the client system context, which is why the script needs to query the server, then the server will supply the resource via this route.
+app.get('/img' , (req, res) => {
+  res.sendFile(__dirname + '/images/sample.png');
 });
 
 app.listen(port, () => {
-    console.log(`Server running at port ${port}`);
+  console.log(`Server running at port ${port}`);
 });
