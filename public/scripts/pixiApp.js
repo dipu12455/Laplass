23ad1a7ae86aa5c6c10dcccc04be3b3a1ea06585
dgiri12 <dgiri1@msudenver.edu>
2023-05-28 22:42:55 -0600

    added an apple rotating on the center, set its anchor point and xy coordinate

--- public/scripts/pixiApp.js
+++ public/scripts/pixiApp.js
@@ -1,13 +1,16 @@
 const app = new PIXI.Application({ width: 640, height: 480, background: '#ffffff' });
 document.body.appendChild(app.view);
 
-let sprite = PIXI.Sprite.from('./images/sample.png');
+let sprite = PIXI.Sprite.from('./images/apple.png');
+sprite.anchor.set(0.5);
+sprite.x = 320;
+sprite.y = 240;
+
 app.stage.addChild(sprite);
 
 // Add a ticker callback to move the sprite back and forth
 let elapsed = 0.0;
 app.ticker.add((delta) => {
-    elapsed += delta;
-    sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
-    });
+    sprite.rotation -= 0.01 * delta;
+});
 
