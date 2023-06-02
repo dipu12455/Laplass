b0301a240d9f4f36925570bec27520d85d611122
dgiri12 <dgiri1@msudenver.edu>
2023-06-02 15:08:38 -0600

    organized code so that draw functions can be defined in user's program (in terms of LP draw calls), then LP uses that compiled draw operations to draw one frame. Loop doesn't work yet, next step implement update function to animate variables. maybe adding drawOperations as child to app is only one time?

--- LPEngine.js
+++ LPEngine.js
@@ -6,7 +6,7 @@ import * as PIXI from 'pixi.js';
 
 // these variables need to be referenced from all functions
 var app;
-var graphics;
+var drawOperations;
 var worldOriginX, worldOriginY, worldDelta;
 
 export function init(_document, _width, _height){
@@ -16,8 +16,7 @@ export function init(_document, _width, _height){
   app = new PIXI.Application({ width: _width, height: _height, background: '#ffffff' });
   _document.body.appendChild(app.view);
 
-  //create a global graphics object to compile drawing functions into
-  graphics = new PIXI.Graphics();
+  drawOperations = new PIXI.Graphics(); //is a graphics object used to collect draw operations from throughout LP. Then, is added as child to app
 }
 
 export function getWorldDelta(){
@@ -36,14 +35,20 @@ export function runTicker(){
   });
 }
 
-export function renderAll(){
-  app.stage.addChild(graphics);
+export function update(){
+  //update
+}
+
+export function draw(_drawFunctions){
+  drawOperations.clear(); //clear drawing of last calls
+  _drawFunctions(); //user defined draw functions called in order, only call draw functions of LP.
+  app.stage.addChild(drawOperations); //drawOperations are compiled, now ready to add it as child
 }
 
 export function draw_line(x1,y1,x2,y2,color){
-  graphics.lineStyle(1, color, 1);
-  graphics.moveTo(worldOriginX+(x1*worldDelta), worldOriginY-(y1*worldDelta));
-  graphics.lineTo(worldOriginX+(x2*worldDelta),worldOriginY-(y2*worldDelta));
+  drawOperations.lineStyle(1, color, 1);
+  drawOperations.moveTo(worldOriginX+(x1*worldDelta), worldOriginY-(y1*worldDelta));
+  drawOperations.lineTo(worldOriginX+(x2*worldDelta),worldOriginY-(y2*worldDelta));
 }
 
 export function draw_screen_grid(_width, _height, _primColor, _secColor){
--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import * as LP from './LPEngine.js';
 
 //these functions need to be run in this exact order
 LP.init(document,640,480);
-LP.draw_screen_grid(50,50,0x000000,0xcccccc);
-LP.draw_line(0,0,5,6,0x000000);
-LP.renderAll();
+
+LP.update(); // update all the objects according to their step functions
+LP.draw(() => {
+  LP.draw_screen_grid(50,50,0x000000,0xcccccc);
+  LP.draw_line(0,0,5,6,0x000000);
+}); // draw operations provided as parameter, only use LP draw functions
+
 LP.runTicker();
