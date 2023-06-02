fc8542d477afb4c4dabba2e3a8817b9d645849f2
dgiri12 <dgiri1@msudenver.edu>
2023-06-01 22:35:19 -0600

    organized pixiapp.js file into better functions. this file is considered the game engine functions container. rename the file later. index.js is implementing game engine functions to run the application. also shows what order the functions need to be executed in, and using asterisk in the import statement

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import {add} from './subindex.js';
-import {runGraphics} from './pixiApp.js';
+import * as lp from './pixiApp.js';
 
-runGraphics(document);
-
+//these functions need to be run in this exact order
+lp.init(document);
+lp.spriteInit();
+lp.lp_draw_line(0,0,250,250,0x000000);
+lp.renderAll();
+lp.runTicker();
--- pixiApp.js
+++ pixiApp.js
@@ -4,21 +4,44 @@
 
 import * as PIXI from 'pixi.js';
 
-export function runGraphics(_document){
-  const app = new PIXI.Application({ width: 640, height: 480, background: '#ffffff' });
+// these variables need to be referenced from all functions
+var app;
+var graphics;
+var sprite; 
 
+export function init(_document){
+  app = new PIXI.Application({ width: 640, height: 480, background: '#ffffff' });
   _document.body.appendChild(app.view);
 
-  let sprite = PIXI.Sprite.from('./images/apple.png');
+  //create a global graphics object to compile drawing functions into
+  graphics = new PIXI.Graphics();
+}
+
+export function spriteInit(){
+  sprite = PIXI.Sprite.from('./images/apple.png');
   sprite.anchor.set(0.5);
   sprite.x = 320;
   sprite.y = 240;
+}
 
-  app.stage.addChild(sprite);
-
+export function runTicker(){
   // Add a ticker callback to move the sprite back and forth
   let elapsed = 0.0;
   app.ticker.add((delta) => {
     sprite.rotation -= 0.01 * delta;
   });
 }
+
+export function renderAll(){
+  app.stage.addChild(sprite);
+  app.stage.addChild(graphics);
+}
+
+export function lp_draw_line(x1,y1,x2,y2,color){
+  graphics.lineStyle(1, color, 1);
+  graphics.moveTo(x1, y1);
+  graphics.lineTo(x2,y2);
+}
+
+
+
