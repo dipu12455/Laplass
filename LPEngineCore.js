0a9785403521f6cd9674302b7ed7308439e829d8
dgiri12 <dgiri1@msudenver.edu>
2023-06-02 23:52:31 -0600

    converted LPEngine to an aggregator file. so now both LPVector and the LPEngineCore (core-functions) are available to index.js in simple namespace

---
+++ LPEngineCore.js
@@ -0,0 +1,85 @@
+import * as PIXI from 'pixi.js';
+
+// these variables need to be referenced from all functions
+var app;
+var drawObject;
+var drawOperations; //variable to hold the user coded draw function
+var worldOriginX, worldOriginY, worldDelta;
+var screenGrid=false;
+var updateOperations;
+
+export function init(_document, _width, _height){
+  worldOriginX = _width/2;
+  worldOriginY = _height/2;
+  worldDelta = 20; //one unit means 20 pixels. so (-5,2) means (-100,40) pixels
+  app = new PIXI.Application({ width: _width, height: _height, background: '#ffffff' });
+  _document.body.appendChild(app.view);
+
+  drawObject = new PIXI.Graphics();
+  app.stage.addChild(drawObject);
+}
+
+export function getWorldDelta(){
+  return worldDelta;
+}
+
+export function setWorldDelta(_worldDelta){
+  worldDelta = _worldDelta;
+}
+
+export function getTicker(){
+  return app.ticker;
+}
+export function defineDrawOperations(_drawOperations){
+  drawOperations = _drawOperations;
+}
+
+export function showScreenGrid(){screenGrid=true;}
+export function hideScreenGrid(){screenGrid=false;}
+
+export function draw(){
+  drawObject.clear(); //clear drawing of last calls
+  if(screenGrid==true){draw_screen_grid(50,50,0x000000,0xcccccc);}
+  drawOperations(); //user defined draw functions called in order, only call draw functions of LP.
+}
+
+export function draw_line(x1,y1,x2,y2,color){
+  drawObject.lineStyle(1, color, 1);
+  drawObject.moveTo(worldOriginX+(x1*worldDelta), worldOriginY-(y1*worldDelta));
+  drawObject.lineTo(worldOriginX+(x2*worldDelta),worldOriginY-(y2*worldDelta));
+}
+
+export function draw_anchor(x,y,color){
+  drawObject.lineStyle(0);
+  drawObject.beginFill(color,1);
+  drawObject.drawCircle(worldOriginX+(x*worldDelta),worldOriginY-(y*worldDelta),2);
+  drawObject.endFill();
+}
+
+export function draw_vector_origin(_v, _lineColor, _anchorColor){
+    draw_line(0,0,_v.getX(),_v.getY(),_lineColor);
+    draw_anchor(_v.getX(),_v.getY(),_anchorColor);
+}
+
+
+function draw_screen_grid(_width, _height, _primColor, _secColor){
+  //draw grid lines
+  //line y-axis lines
+  var n = _height/2;
+  while(true){
+    draw_line(-_width/2,n,_width/2,n,_secColor);
+    n -= 1;
+    if (n < -_height/2) break;
+  }
+  //line y-axis lines
+  n = -_width/2;
+  while(true){
+    draw_line(n,_height/2,n,-_height/2,_secColor);
+    n += 1;
+    if (n > _width/2) break;
+  }
+
+  //draw the origin
+  draw_line(-_width/2,0,_width/2,0,_primColor);
+  draw_line(0,_height/2,0,-_height/2,_primColor);
+}
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import * as LP from './LPEngine.js';
-import * as LPVector from './LPVector.js';
 
 //these functions need to be run in this exact order
 LP.init(document,640,480);
@@ -9,8 +8,8 @@ var animate = 5; //animating this variable
 var elapsed = 0;
 var angle = 0;
 
-var vector1 = new LPVector.LPVector(2,2);
-var vector2 = new LPVector.LPVector(5,5);
+var vector1 = new LP.LPVector(2,2);
+var vector2 = new LP.LPVector(5,5);
 //need to hide implementation of this ticker for this animation, because it's the job of LPEngine and not the client program
 LP.getTicker().add((delta) => {
   elapsed += delta;
@@ -18,11 +17,11 @@ LP.getTicker().add((delta) => {
   animate = Math.cos(elapsed/50.0); //just make it oscillate, divide by 50 is for slowing down animation
   angle += 0.3 * delta;
   
-  var vector1dash = new LPVector.LPVector(0,0); //make a temp vector
+  var vector1dash = new LP.LPVector(0,0); //make a temp vector
   vector1dash.setVector1(vector1.getX()*animate*2.2,vector1.getY()*animate*2.2);
 
-  var vector2dash = new LPVector.LPVector(0,0); //make a temp vector
-  vector2dash.setVector2(LPVector.rotateVector(vector2,angle));
+  var vector2dash = new LP.LPVector(0,0); //make a temp vector
+  vector2dash.setVector2(LP.rotateVector(vector2,angle));
 
   LP.defineDrawOperations(() => {
     LP.draw_vector_origin(vector1dash,0x00ffff,0x0000ff);
