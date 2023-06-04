ebc9de2f4e6ed8c8b1263d88d9c278ef04e7909d
dgiri12 <dgiri1@msudenver.edu>
2023-06-03 22:06:32 -0600

    added a scene file, and exported some functions from it and made it work. now we can have different scenes to render

--- public/scripts/scene01.js
+++ public/scripts/scene01.js
@@ -6,7 +6,8 @@ var angle = 0;
 
 var vector1 = new LP.LPVector(2,2);
 var vector2 = new LP.LPVector(5,5);
-var vector1dash,vector2dash;
+var vector1dash = new LP.LPVector(0,0); //make a temp vector
+var vector2dash = new LP.LPVector(0,0); //make a temp vector
 
 //define a primitive
 var prim01 = new LP.Primitive(0,0);
@@ -27,17 +28,15 @@ export function update(_delta){
   //animation code goes here
   animate = Math.cos(elapsed/50.0); //just make it oscillate, divide by 50 is for slowing down animation
   angle += 0.3 * _delta;
-  
-  var vector1dash = new LP.LPVector(0,0); //make a temp vector
+
   vector1dash.setVector1(vector1.getX()*animate*2.2,vector1.getY()*animate*2.2);
 
-  var vector2dash = new LP.LPVector(0,0); //make a temp vector
   vector2dash.setVector2(LP.rotateVector(vector2,angle));
 }
 export function draw(){
-    LP.draw_primitive(pentagon,0,0,-angle,0x00ff00,0xc9f0e8,true);
-    LP.draw_vector_origin(vector1dash,0x00ffff,0x0000ff);
-    LP.draw_vector_origin(vector2dash,0x00ff00,0xff0000);
-    LP.draw_primitive(prim01,animate*2,animate*-0.6,angle+30,0x0000ff,0x0,true);
+  LP.draw_primitive(pentagon,0,0,-angle,0x00ff00,0xc9f0e8,true);
+  LP.draw_vector_origin(vector1dash,0x00ffff,0x0000ff);
+  LP.draw_vector_origin(vector2dash,0x00ff00,0xff0000);
+  LP.draw_primitive(prim01,animate*2,animate*-0.6,angle+30,0x0000ff,0x0,true);
 }
 
