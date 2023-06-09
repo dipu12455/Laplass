020dd26fb50fe6eed050f1e9509f31bb7e9c3561
dgiri12 <dgiri1@msudenver.edu>
2023-06-09 13:19:39 -0600

    Right now, I am leaving scene_sat3.js as it is, organizing and OOP-fying this code. TODO: I intend to animate all these points. Pentagon and triangle will oscillate towards and away from each other. The coefficients of projections plot on the x-axis animate likewise. The goal is to see if projection onto the axis vector is required? Or can we simply continue with just the coefficients of projection values on the x-axis. Also, the direction of the axis vector doesn't matter, the coefficients still get projected in the same place. If the vector is flipped, the edge points are flipped too, but the x-axis plot appears the same.

--- public/scripts/scene_sat3.js
+++ public/scripts/scene_sat3.js
@@ -1,44 +1,15 @@
 import * as LP from './LPEngine/LPEngine.js';
+import * as S3 from './scene_sat3Def.js';
 
 var elapsed = 0;
 
 //define a primitive
-var prim01 = new LP.Primitive(0,0);
-prim01.add(new LP.LPVector(2,2)); //LPVectors are the so similar to vertices, so just using them
-prim01.add(new LP.LPVector(-2,1));
-prim01.add(new LP.LPVector(-2,-2));
+var prim01 = S3.prim01;
 
 //pentagon
-var pentagon = new LP.Primitive(0,0);
-pentagon.add(new LP.LPVector(-5,1));
-pentagon.add(new LP.LPVector(0,6));
-pentagon.add(new LP.LPVector(5,1));
-pentagon.add(new LP.LPVector(3,-5));
-pentagon.add(new LP.LPVector(-3,-5));
+var pentagon = S3.pentagon;
 
-function projectPrimitiveOntoAxis(_primitive, _axisVector){
-  var i = 0;
-  var projPointList = new LP.LPList();
-  for(i = 0; i < _primitive.getSize(); i += 1){
-    var point = _primitive.get(i);
-
-    const proj = projOfuOnv(point,_axisVector);
-
-    projPointList.add(proj);
-  }
-  return projPointList;
-}
-function coeffOfProjuOnv(_u,_v){
-  const dotProd = LP.dotProduct(_u,_v);
-  const coefficient = dotProd/LP.sqr(_v.getMag());
-  return coefficient; //this is a scalar
-}
-
-function projOfuOnv(_u,_v){
-  const proj = LP.scalarXvector(coeffOfProjuOnv(_u,_v),_v);
-  return proj;
-}
-
+//define the axis we are testing on
 const axis = new LP.LPVector(8,2);
 
 export function update(_delta){
@@ -50,10 +21,10 @@ export function draw(){
   LP.draw_lineV(new LP.LPVector(0,0), axis,0x000000,0x0000ff); //dont wanna see anchor just line
 
   //get a list of coefficients for each point.
-  const pointList1 = testFunc1(pentagon,axis);
+  const pointList1 = S3.getCoefficientsOfProjection(pentagon,axis);
   //get index of the min and max points, to color them differently in both the axisVector plot and the x-axis plot
-  var min = findMin(pointList1);
-  var max = findMax(pointList1);
+  var min = S3.findMin(pointList1);
+  var max = S3.findMax(pointList1);
   
    //plot just the coefficients of projected point onto the x-axis
    var scale = 10; //these coefficients are very small, scale them to make them visible at worldDelta of 20
@@ -91,77 +62,3 @@ export function draw(){
   //LP.draw_primitive(pentagon,0,0,0,0x00ff00,0xc9f0e8,true);
   //LP.draw_primitive(prim01,0,0,0,0x0000ff,0x0,true);
 }
-
-//primitive needs to be defined in vertex list
-function getNormalsOfPrimitive(_primitive){
-  var i = 0;
-  var normalList = new LP.LPList();
-  for (i = 0; i < _primitive.getSize(); i += 1){
-    var p1; var p2;
-    if (i == 0){
-      p1 = pentagon.get(_primitive.getSize()-1);
-      p2 = pentagon.get(i);
-    }
-    else{
-      p1 = pentagon.get(i-1);
-      p2 = pentagon.get(i);
-    }
-
-    var normal = LP.findLeftPerpendicular(LP.v2Minusv1(p1,p2));
-    normalList.add(normal);
-  }
-  return normalList;
-}
-
-function drawNormals(_primitive, _primColor, _secColor){
-  var normals = getNormalsOfPrimitive(_primitive);
-  var i = 0;
-  for (i = 0; i < normals.getSize(); i += 1){
-    LP.draw_vector_origin(normals.get(i),_primColor,_secColor);
-  }
-}
-
-//takes a primitive and only returns a list of the coefficient of each point with the axis
-function testFunc1(_primitive,_axisVector){
-  var i = 0;
-  var pointList = new LP.LPList();
-  for(i = 0; i < _primitive.getSize(); i += 1){
-    var point = _primitive.get(i);
-
-    const proj = coeffOfProjuOnv(point,_axisVector);
-
-    pointList.add(proj);
-  }
-  return pointList;
-}
-
-//takes a list and finds the min, returns the index of the min value of the list
-function findMin(_list /*type LPList*/){ //TODO: what if there are more that one min values? it is possible for two different vertices to have equal projection vector
-  var min = 0;
-  var index = 0;
-  var i = 0;
-  for (i = 0; i < _list.getSize(); i += 1){
-    let value = _list.get(i);
-    if (value < min){
-      min = value;
-      index = i;
-    }
-  }
-  return index;
-}
-
-//takes a list and finds the max, returns the index of the max value of the list
-function findMax(_list){
-  var max = 0;
-  var index = 0;
-  var i = 0;
-  for (i = 0; i < _list.getSize(); i += 1){
-    let value = _list.get(i);
-    if (value > max){
-      max = value;
-      index = i;
-    }
-  }
-  return index;
-}
-
---
+++ public/scripts/scene_sat3Def.js
@@ -0,0 +1,115 @@
+import * as LP from './LPEngine/LPEngine.js';
+
+//define a primitive
+export var prim01 = new LP.Primitive(0,0);
+prim01.add(new LP.LPVector(2,2)); //LPVectors are the so similar to vertices, so just using them
+prim01.add(new LP.LPVector(-2,1));
+prim01.add(new LP.LPVector(-2,-2));
+
+//pentagon
+export var pentagon = new LP.Primitive(0,0);
+pentagon.add(new LP.LPVector(-5,1));
+pentagon.add(new LP.LPVector(0,6));
+pentagon.add(new LP.LPVector(5,1));
+pentagon.add(new LP.LPVector(3,-5));
+pentagon.add(new LP.LPVector(-3,-5));
+
+//returns a list of vectors that are projected on the given axisVector
+export function projectPrimitiveOntoAxis(_primitive, _axisVector){
+  var i = 0;
+  var projPointList = new LP.LPList();
+  for(i = 0; i < _primitive.getSize(); i += 1){
+    var point = _primitive.get(i);
+
+    const proj = projOfuOnv(point,_axisVector);
+
+    projPointList.add(proj);
+  }
+  return projPointList;
+}
+
+//takes a primitive and only returns a list of the coefficient of each point with the axis
+export function getCoefficientsOfProjection(_primitive,_axisVector){
+  var i = 0;
+  var pointList = new LP.LPList();
+  for(i = 0; i < _primitive.getSize(); i += 1){
+    var point = _primitive.get(i);
+
+    const proj = coeffOfProjuOnv(point,_axisVector);
+
+    pointList.add(proj);
+  }
+  return pointList;
+}
+
+//takes a list and finds the min, returns the index of the min value of the list
+export function findMin(_list /*type LPList*/){ //TODO: what if there are more that one min values? it is possible for two different vertices to have equal projection vector
+  var min = 0;
+  var index = 0;
+  var i = 0;
+  for (i = 0; i < _list.getSize(); i += 1){
+    let value = _list.get(i);
+    if (value < min){
+      min = value;
+      index = i;
+    }
+  }
+  return index;
+}
+
+//takes a list and finds the max, returns the index of the max value of the list
+export function findMax(_list){
+  var max = 0;
+  var index = 0;
+  var i = 0;
+  for (i = 0; i < _list.getSize(); i += 1){
+    let value = _list.get(i);
+    if (value > max){
+      max = value;
+      index = i;
+    }
+  }
+  return index;
+}
+
+function projOfuOnv(_u,_v){
+  const proj = LP.scalarXvector(coeffOfProjuOnv(_u,_v),_v);
+  return proj;
+}
+
+function coeffOfProjuOnv(_u,_v){
+  const dotProd = LP.dotProduct(_u,_v);
+  const coefficient = dotProd/LP.sqr(_v.getMag());
+  return coefficient; //this is a scalar
+}
+
+//primitive needs to be defined in vertex list
+function getNormalsOfPrimitive(_primitive){
+  var i = 0;
+  var normalList = new LP.LPList();
+  for (i = 0; i < _primitive.getSize(); i += 1){
+    var p1; var p2;
+    if (i == 0){
+      p1 = pentagon.get(_primitive.getSize()-1);
+      p2 = pentagon.get(i);
+    }
+    else{
+      p1 = pentagon.get(i-1);
+      p2 = pentagon.get(i);
+    }
+
+    var normal = LP.findLeftPerpendicular(LP.v2Minusv1(p1,p2));
+    normalList.add(normal);
+  }
+  return normalList;
+}
+
+//put inside the draw function of a scene
+function drawNormals(_primitive, _primColor, _secColor){
+  var normals = getNormalsOfPrimitive(_primitive);
+  var i = 0;
+  for (i = 0; i < normals.getSize(); i += 1){
+    LP.draw_vector_origin(normals.get(i),_primColor,_secColor);
+  }
+}
+
