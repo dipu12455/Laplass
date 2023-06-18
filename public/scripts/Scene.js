5ba9ebe7202df819bd81276513c8cdc06b761022
dgiri12 <dgiri1@msudenver.edu>
2023-06-17 22:06:09 -0600

    commit

--- public/scripts/Scene.js
+++ public/scripts/Scene.js
@@ -37,16 +37,14 @@ export function draw() {
   //obtain the first primitive transformed into the orientation of its instance
   LP.selectInstance(ins1);
   var prim1 = LP.transform_primitive(LP.getPrimitive(LP.getPrimitiveIndex()),
-    LP.getX(),
-    LP.getY(),
-    LP.getRot()); LP.unSelectAll();
+    LP.getX(),LP.getY(),LP.getRot());
+    LP.unSelectAll();
 
   //obtain the second primitive tranformed into the orientation of its instance
   LP.selectInstance(ins2);
   var prim2 = LP.transform_primitive(LP.getPrimitive(LP.getPrimitiveIndex()),
-    LP.getX(),
-    LP.getY(),
-    LP.getRot()); LP.unSelectAll();
+  LP.getX(),LP.getY(),LP.getRot()); 
+  LP.unSelectAll();
 
   //check collision between these two primitives, and if so draw a red dot
   if (LP.checkCollision(prim1, prim2)) {
@@ -62,9 +60,7 @@ export function draw() {
 
 function draw_instance() {
   var trans = LP.transform_primitive(LP.getPrimitive(LP.getPrimitiveIndex()),
-    LP.getX(),
-    LP.getY(),
-    LP.getRot());
+  LP.getX(),LP.getY(),LP.getRot());
 
   LP.draw_primitive(trans);//, 0x00ff00, 0xc9f0e8, true);
 }
