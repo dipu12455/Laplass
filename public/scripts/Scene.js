8187ace0e9063fbd6e95b585754a61b4e5e13512
dipu12455 <dipu12455@gmail.com>
2023-06-25 21:40:52 -0600

    Update Scene.js

--- public/scripts/Scene.js
+++ public/scripts/Scene.js
@@ -1,6 +1,9 @@
 import * as LP from './LPEngine/LPEngine.js';
 import { pCircle } from './properties/pCircle.js';
+import { pDrawObject } from './properties/pDrawObject.js';
+
+var drawObject = LP.addInstance(-1,-1,pDrawObject);
 
 var circle1 = LP.addInstance(-1,-1,pCircle);
 LP.selectInstance(circle1);
-LP.setPosition(0,0); LP.unSelectAll();
+LP.setPosition(-5,0); LP.unSelectAll();
