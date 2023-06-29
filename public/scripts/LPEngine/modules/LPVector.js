d807d9d4bd626ddd275f3283a17ce2db522a3327
dipu12455 <dipu12455@gmail.com>
2023-06-29 12:52:39 -0600

    finished writing unit tests of LPVector

--- public/scripts/LPEngine/modules/LPVector.js
+++ public/scripts/LPEngine/modules/LPVector.js
@@ -1,41 +1,4 @@
-export function sqr(_num) { return _num * _num; }
-export function radtodeg(_radians) { return _radians * (180 / Math.PI); }
-export function degtorad(_degrees) { return _degrees * (Math.PI / 180); }
-
 //all vectors are expressed as an array of the form [x,y].
-
-export function getTheta(_v) {
-  return radtodeg(Math.atan2(_v[1], _v[0]));
-}
-export function getMag(_v) {
-  return Math.sqrt(sumOfSqr(_v[0],_v[1]));
-}
-
-export function rotateVector(_v, _theta) {
-  var vmag = getMag(_v);
-  var xdash = vmag * Math.cos(degtorad(_theta));
-  var ydash = vmag * Math.sin(degtorad(_theta));
-  return [xdash, ydash];
-}
-
-export function transformVector(_v, _x, _y, _theta) {
-  var v = rotateVector(_v, _theta); //first rotate
-  return [v[0] + _x, v[1] + _y]; //then translate
-}
-
-//a unit vector is used to indicate direction
-export function getUnitVector(_v) {
-  var mag = getMag(_v);
-  if (mag <= 0) return [0, 0]; //avoid divide by 0
-  var i = _v[0] / mag;
-  var j = _v[1] / mag;
-  return [i, j];
-}
-
-export function getVectorRTHeta(_r, _theta) { //theta in degrees
-  return [_r * Math.cos(degtorad(_theta)), _r * Math.sin(degtorad(_theta))];
-}
-
 export function dotProduct(_v1, _v2) {
   return (_v1[0] * _v2[0]) + (_v1[1] * _v2[1]);
 }
@@ -56,6 +19,31 @@ export function findLeftPerpendicular(_v) {
   return [-_v[1], _v[0]];
 }
 
+export function findAverage(_v1, _v2) {
+  return [(_v1[0] + _v2[0]) / 2, (_v1[1] + _v2[1]) / 2];
+}
+
+export function radtodeg(_radians) { return _radians * (180 / Math.PI); }
+
+export function degtorad(_degrees) { return _degrees * (Math.PI / 180); }
+
+export function sqr(_num) { return _num * _num; }
+
+export function sumOfSqr(_n1,_n2){
+  return (sqr(_n1)+sqr(_n2));
+}
+
+export function getTheta(_v) {
+  return radtodeg(Math.atan2(_v[1], _v[0]));
+}
+export function getMag(_v) {
+  return Math.sqrt(sumOfSqr(_v[0],_v[1]));
+}
+
+export function getVectorRTHeta(_r, _theta) { //theta in degrees
+  return [_r * Math.cos(degtorad(_theta)), _r * Math.sin(degtorad(_theta))];
+}
+
 export function isVectorWithinRange(_v, low, high) {
   var mag = getMag(_v);
   if (mag >= low && mag <= high) {
@@ -64,11 +52,23 @@ export function isVectorWithinRange(_v, low, high) {
   return false;
 }
 
-export function findAverage(_v1, _v2) {
-  return [(_v1[0] + _v2[0]) / 2, (_v1[1] + _v2[1]) / 2];
+export function rotateVector(_v, _theta) {
+  var vmag = getMag(_v);
+  var xdash = vmag * Math.cos(degtorad(_theta));
+  var ydash = vmag * Math.sin(degtorad(_theta));
+  return [xdash, ydash];
 }
 
-export function sumOfSqr(_n1,_n2){
-  return (sqr(_n1)+sqr(_n2));
+//a unit vector is used to indicate direction
+export function getUnitVector(_v) {
+  var mag = getMag(_v);
+  if (mag <= 0) return [0, 0]; //avoid divide by 0
+  var i = _v[0] / mag;
+  var j = _v[1] / mag;
+  return [i, j];
 }
-//unit tests
+
+export function transformVector(_v, _x, _y, _theta) {
+  var v = rotateVector(_v, _theta); //first rotate
+  return [v[0] + _x, v[1] + _y]; //then translate
+}
--- public/scripts/properties/drawObject.js
+++ public/scripts/properties/drawObject.js
@@ -7,6 +7,7 @@ var update = (_delta) => {
 }
 
 var draw = () => {
+    LP.draw_anchor(LP.rotateVector([0,1],90),0xff0000);
 }
 
 export const pDrawObject = LP.addProperty(init, update, draw);
