1e62963eb1b74a203769c48a474f4e4c54b1d0e1
dipu12455 <dipu12455@gmail.com>
2024-07-06 14:47:48 -0600

    Fixed crashing at startup, now Laplass will specifically import pixiJS version 7.4.2

---
+++ codeAside.js
@@ -0,0 +1,15 @@
+
+<head>
+  <!--put the PIXIjs script-->
+  <script src="https://pixijs.download/release/pixi.min.js"></script>
+  <!--This is the initialization scripts for Threejs-->
+  <script async src="https://unpkg.com/es-module-shims@1.6.3/dist/es-module-shims.js"></script>
+
+  <script type="importmap">
+      {
+        "imports": {
+          "three": "https://unpkg.com/three/build/three.module.js",
+          "three/addons/": "https://unpkg.com/three/examples/jsm/"
+        }
+      }
+    </script>
